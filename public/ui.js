7485164ff21745b51994f2eb90acc14fb6d02c34
Poll Castillo <alejandrocordovafree@gmail.com>
2022-08-08 12:47:09 -0500

    feat: data dropdown

--- public/ui.js
+++ public/ui.js
@@ -22,15 +22,21 @@ console.clear();
 /* --------------------------------------
   DATE AND TIME
  -------------------------------------- */
+/*
+  Get the month an date objects
+  from HTML  
+*/
 const UImonth = document.getElementById('UImonth');
 const UIdate = document.getElementById('UIdate');
+// Creating date object
 const $D = new Date();
-
+// Get the current day
 let day = $D.getDay();
+// Get the current month
 let month = $D.getMonth();
+// Get the current date
 let date = $D.getDate();
-
-// Month name
+// Change month name depending of the number
 switch (month) {
   case 0:
     month = 'Enero'
@@ -80,7 +86,7 @@ switch (month) {
     month = 'Diciembre'
   break;
 }
-// Day names
+// Change day name depending of the number
 switch (day) {
   case 0:
     day = 'Domingo';
@@ -110,19 +116,20 @@ switch (day) {
     day = 'Sábado';
   break;
 }
-
+// Write the current date into HTML
 UImonth.innerHTML = month;
 UIdate.innerHTML = `${day} ${date}`;
 
 /* --------------------------------------
- TABLE STATUS
- -------------------------------------- */
+  TABLE STATUS
+----------------------------------------- */
+// Get the status from HTML
 const statusDOM = document.querySelectorAll('#tableStatus');
-
+// Index all status item to change class
 for (let i = 0; i < statusDOM.length; i++) {
-  // ADD BASIC STYLE TO STATUS
+  // Add the base status to tableStatus
   statusDOM[i].classList.add('table__status');
-
+  // Get the text content from status to choose status type
   const status = statusDOM[i].innerHTML;
   if (status == 'confirmado' | status == 'Confirmado') {
     statusDOM[i].classList.add('table__status--c');
@@ -134,3 +141,57 @@ for (let i = 0; i < statusDOM.length; i++) {
     statusDOM[i].innerHTML = 'Activo'
   }
 }
+
+/* --------------------------------------
+ DROPDOWNS
+----------------------------------------- */
+
+//Get all dropdowns from the document
+const dropdowns = document.querySelectorAll('.dropdown');
+
+// Loop through al dropdown elements
+dropdowns.forEach((dropdown) => {
+  // Get inner lements from each dropdown
+  const select = dropdown.querySelector('.select');
+  const caret = dropdown.querySelector('.caret');
+  const menu = dropdown.querySelector('.menu');
+  const options = dropdown.querySelectorAll('.menu li');
+  const selected = dropdown.querySelector('.selected');
+
+  /*
+    We are using this method in order to have
+    multiple dropdowns menus on the page work
+  */
+
+  // Add a click event to the select element
+  select.addEventListener('click', ()=> {
+    // Add the clicked select styles to the select element
+    select.classList.toggle('select--isClicked');
+    // Add the rotate styles to the caret element
+    caret.classList.toggle('caret--isRotate');
+    // Add the open styles to the menu element
+    menu.classList.toggle('menu--isOpen');
+  });
+
+  // Loop through all option elements
+  options.forEach((option)=> {
+    // Add a click event to the option element
+    option.addEventListener('click', ()=> {
+      // Change selected inner text to clicked option inner text
+      selected.innerText = option.innerText;
+      // Add the clicked select styles to the select element
+      select.classList.remove('select--isClicked');
+      // Add the rotate styles to the caret element;
+      caret.classList.remove('caret--isRotate');
+      // Add the open styles to the menu element
+      menu.classList.remove('menu--isOpen');
+      // Remove active class from all option elements
+      options.forEach((option) => {
+        option.classList.remove('active');
+      });
+      // Add active class to clicked option element
+      option.classList.add('active');
+    });
+  });
+});
+
